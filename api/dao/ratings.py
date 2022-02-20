b8e2960ce850efff64e87f3cf35f640b0c831229
kenichi-lon <kenichi.moriwaki.uk@gmail.com>
2022-02-20 15:21:28 +0000

    added python app

---
+++ api/dao/ratings.py
@@ -0,0 +1,66 @@
+from api.data import ratings
+from api.exceptions.notfound import NotFoundException
+
+from api.data import goodfellas
+
+
+class RatingDAO:
+    """
+    The constructor expects an instance of the Neo4j Driver, which will be
+    used to interact with Neo4j.
+    """
+    def __init__(self, driver):
+        self.driver=driver
+
+    """
+    Add a relationship between a User and Movie with a `rating` property.
+    The `rating` parameter should be converted to a Neo4j Integer.
+    """
+    # tag::add[]
+    def add(self, user_id, movie_id, rating):
+        # tag::create_rating[]
+        # Create function to save the rating in the database
+        def create_rating(tx, user_id, movie_id, rating):
+            return tx.run("""
+            MATCH (u:User {userId: $user_id})
+            MATCH (m:Movie {tmdbId: $movie_id})
+            MERGE (u)-[r:RATED]->(m)
+            SET r.rating = $rating,
+                r.timestamp = timestamp()
+            RETURN m {
+                .*,
+                rating: r.rating
+            } AS movie
+            """, user_id=user_id, movie_id=movie_id, rating=rating).single()
+        # end::create_rating[]
+
+        # tag::run_create_rating[]
+        with self.driver.session() as session:
+            record = session.write_transaction(create_rating, user_id=user_id, movie_id=movie_id, rating=rating)
+        # end::run_create_rating[]
+
+            # tag::not_found[]
+            if record is None:
+                raise NotFoundException()
+            # end::not_found[]
+
+            # tag::return[]
+            return record["movie"]
+            # end::return[]
+    # end::add[]
+
+
+    """
+    Return a paginated list of reviews for a Movie.
+    Results should be ordered by the `sort` parameter, and in the direction specified
+    in the `order` parameter.
+    Results should be limited to the number passed as `limit`.
+    The `skip` variable should be used to skip a certain number of rows.
+    """
+    # tag::forMovie[]
+    def for_movie(self, id, sort = 'timestamp', order = 'ASC', limit = 6, skip = 0):
+        # TODO: Get ratings for a Movie
+        # TODO: Remember to escape the braces in the cypher query with double braces: {{ }}
+
+        return ratings
+    # end::forMovie[]
